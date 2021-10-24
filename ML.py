4beadaa8583cb763ec1a84635553c64e0838ef67
richardmuench <richard.muench.15@cnu.edu>
2021-10-24 01:02:28 -0400

    Update ML.py
    Test to work on github

--- ML.py
+++ ML.py
@@ -1,7 +1,7 @@
 """
 The big boy, integrate ML learning features to the bot to push it towards "perfect" play.
 A successfull implementation should result in a two bots finishing the game with a tie on
-every single bout.    
+every single bout. You know eventually.
 
 @Author John Ulmer
 @Author Richard Muench
